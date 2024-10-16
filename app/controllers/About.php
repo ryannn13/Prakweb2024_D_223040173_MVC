392e559473d15e08861d118c0096ea5062a5ca16
ryannn13 <ryan.hidayat0604@gmail.com>
2024-10-16 15:27:55 +0700

    View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,14 +1,24 @@
 <?php
 
-class About
+class About extends Controller
 {
-    public function index($nama = 'Ryan', $pekerjaan = 'Mahasiswa')
+    public function index($nama = 'Ryan', $pekerjaan = 'Mahasiswa', $umur = 20)
     {
-        echo "Halo, nama saya $nama, Saya adalah seorang $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page()
     {
-        echo "About/page";
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+    <h1>About Me</h1>
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> Tahun, saya seorang <?= $data['pekerjaan']; ?></p>
