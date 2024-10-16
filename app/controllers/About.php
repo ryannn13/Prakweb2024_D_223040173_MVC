597fab06a349121ab710455494554e0116d4db85
ryannn13 <ryan.hidayat0604@gmail.com>
2024-10-16 14:27:01 +0700

    Controller

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About
+{
+    public function index($nama = 'Ryan', $pekerjaan = 'Mahasiswa')
+    {
+        echo "Halo, nama saya $nama, Saya adalah seorang $pekerjaan";
+    }
+
+    public function page()
+    {
+        echo "About/page";
+    }
+}
