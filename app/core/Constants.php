0bac67de76631395da03ab7a5f9d7fe80afbd385
ryannn13 <ryan.hidayat0604@gmail.com>
2024-10-16 16:22:53 +0700

    Assets

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/Prakweb2024_D_223040173_MVC/public/');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>img/ryan.jpg" alt="Ryan Hidayat" width="250" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> Tahun, saya seorang <?= $data['pekerjaan']; ?></p>
+</div>
