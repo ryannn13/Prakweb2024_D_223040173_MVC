8bcabccc81614c935bc50a127093c1f374822c91
ryannn13 <ryan.hidayat0604@gmail.com>
2024-10-16 20:28:30 +0700

    Database Wrapper

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/Prakweb2024_D_223040173_MVC/public/');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'prakweb2024_d_223040173_mvc');
--- app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/Prakweb2024_D_223040173_MVC/public/');
