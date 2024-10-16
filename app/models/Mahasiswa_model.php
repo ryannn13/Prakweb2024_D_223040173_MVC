68a307b7dc3b9f0f7bcf3671164a34e18ed5e71c
ryannn13 <ryan.hidayat0604@gmail.com>
2024-10-16 17:17:43 +0700

    Model

---
+++ app/models/Mahasiswa_model.php
@@ -0,0 +1,26 @@
+<?php
+
+class Mahasiswa_model
+{
+    private $dbh;
+    private $stmt;
+
+    public function __construct()
+    {
+        //data source name
+        $dsn = 'mysql:host=localhost;dbname=prakweb2024_d_223040173_mvc';
+
+        try {
+            $this->dbh = new PDO($dsn, 'root', '');
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function getAllMahasiswa()
+    {
+        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+        $this->stmt->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+    private $nama = 'Ryan Hidayat';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
